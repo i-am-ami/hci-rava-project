21bc5805ecd6e0f6ac236158ba5222148cbb92a5
Abbhinav Jayaraman <jayabbhi@gmail.com>
2025-03-19 18:56:22 -0500

    I learned how to use dotenv properly, can't believe it took this long

--- gpt.py
+++ gpt.py
@@ -2,23 +2,15 @@ import os
 from openai import AzureOpenAI
 from dotenv import load_dotenv
 
-endpoint = os.getenv('GPT_ENDPOINT')
 model_name = "gpt-35-turbo"
 deployment = "gpt-35-turbo"
 
-subscription_key = os.getenv('GPT_KEY')
 api_version = "2025-01-01-preview"
-# gpt_key = 
-# print(f'GPT_KEY: {gpt_key}')
-# gpt_endpoint = os.getenv('GPT_ENDPOINT')
-# print(f'GPT_ENDPOINT: {gpt_endpoint}')
-# gpt_region = os.getenv('GPT_REGION')
-# print(f'GPT_REGION: {gpt_region}')
+
+load_dotenv()
 
 client = AzureOpenAI(
     api_version=api_version,
-    azure_endpoint=endpoint,
-    api_key=subscription_key,
 )
 
 
