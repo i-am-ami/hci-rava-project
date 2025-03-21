8b450377fb270740900dbcab402f17e3dc6f16d9
Abbhinav Jayaraman <jayabbhi@gmail.com>
2025-03-20 20:10:15 -0500

    requirements.txt update, non-threaded logging that is inaccurate for misunderstandings.

--- rava_app.py
+++ rava_app.py
@@ -13,9 +13,9 @@ from rava_backend import recognize_speech, generate_response, speak_response, ca
 
 
 def allowSelfSignedHttps(allowed):
-    # bypass the server certificate verification on client side
-    if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
-        ssl._create_default_https_context = ssl._create_unverified_context
+  # bypass the server certificate verification on client side
+  if allowed and not os.environ.get('PYTHONHTTPSVERIFY', '') and getattr(ssl, '_create_unverified_context', None):
+    ssl._create_default_https_context = ssl._create_unverified_context
 
 def main():
     # Agent Speech Rate Percentage. Initialized as zero to serve a s a flag that usre has not spoken yet
@@ -23,191 +23,193 @@ def main():
     # Tell them to project their voice as if they are presenting something, and keeping a good (1ft) distance from
     #  their mics, possibly?
     if "agent_sr_p" not in st.session_state:
-        st.session_state.agent_sr_p = 0
-        st.session_state.agent_sr = 0
+      st.session_state.agent_sr_p = 0
+      st.session_state.agent_sr = 0
     
     st.set_page_config(page_title="Speech To Text", layout="wide")
 
     st.title('Rava App')
     if st.session_state.agent_sr_p == 0:
-        st.subheader('Speak as if you are presenting and project your voice, so the program can understand you.')
+      st.subheader('Speak as if you are presenting and project your voice, so the program can understand you.')
         
     allowSelfSignedHttps(True)
     if "agent_status" not in st.session_state:
-        st.session_state.agent_status = "inactive"
+      st.session_state.agent_status = "inactive"
 
     # Custom CSS to center the button and make it circular
     st.markdown("""
-        <style>
-        div.stButton>button:first-child {
-            display: block;
-            margin: 0 auto;
-            justify-content: center;
-            align-items: center;
-            width: 160px;
-            height: 80px;
-            font-size: 20px;
-            background-color:;
-        }
-        div.stButton>button:second-child {
-            display: block;
-            margin-top: 10px;
-            justify-content: center;
-            align-items: center;
-            font-size: 12px;
-        }
-        div.stColumns columns {
-            display: block; 
-            margin: 0 auto;
-        }
+      <style>
+      div.stButton>button:first-child {
+        display: block;
+        margin: 0 auto;
+        justify-content: center;
+        align-items: center;
+        width: 160px;
+        height: 80px;
+        font-size: 20px;
+        background-color:;
+      }
+      div.stButton>button:second-child {
+        display: block;
+        margin-top: 10px;
+        justify-content: center;
+        align-items: center;
+        font-size: 12px;
+      }
+      div.stColumns columns {
+        display: block; 
+        margin: 0 auto;
+      }
 
-        </style>
+      </style>
     """, unsafe_allow_html=True)
     
     col1, recordCol, buttonCol, col3 = st.columns(4)
 
     if "agent_history" not in st.session_state:
-        st.session_state.agent_history = {"User Information":[], "Agent Information":[]}
+      st.session_state.agent_history = {"User Information":[], "Agent Information":[]}
     
     if "messages" not in st.session_state:
-        st.session_state.messages = [{"role": "system", "content": "You are a helpful assistant."}]
+      st.session_state.messages = [{"role": "system", "content": "You are a helpful assistant."}]
 
     if "event_log" not in st.session_state:
-        st.session_state.event_log = pd.DataFrame(columns=["Timestamp", "Message"])
+      st.session_state.event_log = pd.DataFrame(columns=["Timestamp", "Message"])
+
+    if "m_log" not in st.session_state:
+      st.session_state.m_log = pd.DataFrame(columns=["Timestamp", "Message"])
 
     if "m_log_status" not in st.session_state:
-        st.session_state.m_log_status = False
-
-    if "user_recording_status" not in st.session_state:
-        st.session_state.m_log_status = False
-
-
+      st.session_state.m_log_status = False
 
     agent_status_message = st.empty()
 
     with recordCol:
-        audio = audiorecorder("Click to record", "Click to stop recording", "Click to pause",
-            custom_style={
-                "display": "block",
-                "justifyContent": "center",
-                "alignItems": "center",
-                "margin": "0 auto",
-                "width": "160px",
-                "height": "80px",
-                "fontSize": "20px",
-                "color": "white",
-                "border": "none",
-                "borderRadius": "7px"
-            },
-            start_style={
-                "backgroundColor": "green",
-            },
-            pause_style={
-                "backgroundColor": "orange",
-            },
-            stop_style={
-                "backgroundColor": "red",
-                "margin-top": "10px",
-            },
-            key="audio"
-        )
+      audio = audiorecorder("Click to record", "Click to stop recording", "Click to pause",
+        custom_style={
+          "display": "block",
+          "justifyContent": "center",
+          "alignItems": "center",
+          "margin": "0 auto",
+          "width": "160px",
+          "height": "80px",
+          "fontSize": "20px",
+          "color": "white",
+          "border": "none",
+          "borderRadius": "7px"
+        },
+        start_style={
+            "backgroundColor": "green",
+        },
+        pause_style={
+            "backgroundColor": "orange",
+        },
+        stop_style={
+            "backgroundColor": "red",
+            "margin-top": "10px",
+        },
+        key="audio"
+      )
 
 
     with buttonCol:	
-        send_button = st.button("Send to RAVA", key="send", on_click=set_agent_state, args=("waiting",), disabled=(st.session_state.agent_status != "inactive"))
-        end_button = st.button("End Conversation", key="end", on_click=set_agent_state, args=("inactive",), disabled =st.session_state.agent_status == "inactive")
-        m_log_button = st.button("Note a marker for misunderstanding", key="tag", on_click=log_misunderstanding, args=(), disabled =(st.session_state.agent_status == "inactive" or 
-                                                                                                                                        st.session_state.agent_status == "waiting"))
-        if send_button:
-            if len(audio) > 0:
-                audio.export(out_f = "user_input.wav", format = "wav")                    
-                rava()
-                st.write(st.session_state.agent_history)
-                st.session_state.agent_history = {"User Information":[], "Agent Information":[]}
-            else :
-                agent_status_message.text("No recording file detected. Try recording again.")
-        
-        if end_button:
-            st.write(st.session_state.agent_history)
-            st.session_state.agent_history = {"User Information":[], "Agent Information":[]}
-            st.write("Conversation ended.")
-            csv_file = io.StringIO()
-            st.session_state.event_log.to_csv(csv_file, index=False)
-            csv_data = csv_file.getvalue()
-            st.download_button(
-                label="Download Log (CSV)",
-                data=csv_data,
-                file_name="app_log.csv",
-                mime="text/csv"
-            )
-        if m_log_button:
-            st.write("Noting mark.")
+      send_button = st.button("Send to RAVA", key="send", on_click=set_agent_state, args=("waiting",), disabled=(st.session_state.agent_status != "inactive"))
+      end_button = st.button("End Conversation", key="end", on_click=set_agent_state, args=("inactive",), disabled = st.session_state.agent_status == "inactive")
+      m_log_button = st.button("Log a misunderstanding", key="tag", on_click=log_misunderstanding, args=(), disabled = st.session_state.agent_status == "inactive")
+      if send_button:
+        if len(audio) > 0:
+          audio.export(out_f = "user_input.wav", format = "wav")                    
+          rava()
+          st.write(st.session_state.agent_history)
+          st.session_state.agent_history = {"User Information":[], "Agent Information":[]}
+        else :
+          agent_status_message.text("No recording file detected. Try recording again.")
+      
+      if end_button:
+        st.write(st.session_state.agent_history)
+        st.session_state.agent_history = {"User Information":[], "Agent Information":[]}
+        st.write("Conversation ended.")
+
+        combined_log = pd.concat([st.session_state.m_log, st.session_state.event_log], ignore_index=True)
+        final_log = combined_log.sort_values(by=combined_log.columns[0])
+
+        csv_file = io.StringIO()
+        final_log.to_csv(csv_file, index=False)
+        csv_data = csv_file.getvalue()
+
+        st.download_button(
+            label="Download App Log (CSV)",
+            data=csv_data,
+            file_name="app_log.csv",
+            mime="text/csv"
+        )
 
 def user_recording_present():
-    my_file = Path("./user_input.wav")
-    return my_file.is_file()
+  my_file = Path("./user_input.wav")
+  return my_file.is_file()
 
-def log_stamp(message):
-    timestamp = datetime.now()
-    st.session_state.event_log = pd.concat([st.session_state.event_log, 
-                                              pd.DataFrame({"Timestamp": [timestamp], 
-                                                            "Message": [message]})], 
-                                                            ignore_index=True)
+def log_stamp(misunderstanding, message):
+  timestamp = datetime.now()
+  log_data = pd.DataFrame({"Timestamp": [timestamp], "Message": [message]})
+
+  if misunderstanding:
+    st.session_state.m_log = pd.concat([st.session_state.m_log, log_data], ignore_index=True)
+  else:
+    st.session_state.event_log = pd.concat([st.session_state.event_log, log_data], ignore_index=True)
 
 def log_misunderstanding():
-    timestamp = datetime.now()
-    st.session_state.m_log_status = not st.session_state.m_log_status
+  timestamp = datetime.now()
+  st.session_state.m_log_status = not st.session_state.m_log_status
 
-    # True = Start of the misunderstanding, False = end of the misunderstanding
-    if st.session_state.m_log_status :
-        log_stamp("Start of user misunderstanding")
-    else: 
-        log_stamp("End of user misunderstanding")
+  # True = Start of the misunderstanding, False = end of the misunderstanding
+  if st.session_state.m_log_status :
+    log_stamp(True, "Start of user misunderstanding")
+  else: 
+    log_stamp(True, "End of user misunderstanding")
 
 def set_agent_state(s):
-    st.session_state["agent_status"] = s 
+  st.session_state["agent_status"] = s 
 
 def rava():
     
     if "agent_status" not in st.session_state:
-        st.session_state.agent_status = "waiting"
+      st.session_state.agent_status = "waiting"
 
     """Main voice agent loop."""
     agent_status_message = st.empty()
 
-    log_stamp("Started recording user")
+    log_stamp(False, "Started recording user")
     user_sr, user_input_text = recognize_speech(st.session_state.agent_history)
-    log_stamp(f'Finished recording user, speech rate: {user_sr} (syllables/sec), input: {user_input_text}')
+    log_stamp(False, f'Finished recording user, speech rate: {user_sr} (syllables/sec), input: {user_input_text}')
     agent_status_message = st.empty()
 
     if user_input_text:
-        st.session_state.agent_status = "responding"
+      st.session_state.agent_status = "responding"
     else:
-        st.session_state.agent_status = "waiting"
+      st.session_state.agent_status = "waiting"
         
     if st.session_state.agent_status == "responding":
-        agent_status_message.text("Agent responding...")
-        response = generate_response(user_input_text, st.session_state.messages) # still working on this
-        # response = "Voila une reponse"
-        
+      agent_status_message.text("Agent responding...")
+      response = generate_response(user_input_text, st.session_state.messages) # still working on this
+      # response = "Voila une reponse"
+      
 
-        if st.session_state.agent_sr_p == 0:
-            st.session_state.agent_sr, st.session_state.agent_sr_p = calc_new_sr_p(5, 8, user_sr)
-        else:
-            st.session_state.agent_sr, st.session_state.agent_sr_p = calc_new_sr_p(st.session_state.agent_sr_p,
-                                                                                   st.session_state.agent_sr, 
-                                                                                   user_sr)
-        log_stamp(f'Agent speaking to user: {response}, with speech rate of {st.session_state.agent_sr}')
-        speak_response(response, st.session_state.agent_sr_p)
-        log_stamp(f'Agent done speaking to user')
-        agent_status_message.text("")
+      if st.session_state.agent_sr_p == 0:
+        st.session_state.agent_sr, st.session_state.agent_sr_p = calc_new_sr_p(5, 8, user_sr)
+      else:
+        st.session_state.agent_sr, st.session_state.agent_sr_p = calc_new_sr_p(st.session_state.agent_sr_p,
+                                                                               st.session_state.agent_sr, 
+                                                                               user_sr)
+
+      # print(f'Debugging: {st.session_state.agent_status} is barring noting a mark for misunderstanding')
+      log_stamp(False, f'Agent starts speaking: {response}, speech rate: {st.session_state.agent_sr}, synth rate: {st.session_state.agent_sr_p}')
+      speak_response(response, st.session_state.agent_sr_p)
+      log_stamp(False, f'Agent done speaking to user')
+      agent_status_message.text("")
     elif st.session_state.agent_status == "waiting":
-        agent_status_message.text("No input detected. Try recording again.")
+      agent_status_message.text("No input detected. Try recording again.")
     else:
-        pass;
+      pass;
     set_agent_state("inactive")
-    # st.write(st.session_state.agent_history)
-    # return st.session_state.agent_history
 
 if __name__ == "__main__":
     main()
--- rava_backend.py
+++ rava_backend.py
@@ -44,53 +44,53 @@ speech_config = speechsdk.SpeechConfig(subscription=speech_key, region=speech_re
 from pydub import AudioSegment
 
 def recognize_speech(agent_history):
-    user_input_text = "";
+  user_input_text = "";
 
-    audio = AudioSegment.from_wav("user_input.wav")
+  audio = AudioSegment.from_wav("user_input.wav")
 
-    # Set the desired sample rate and bit depth
-    desired_sample_rate = 16000  # 16 kHz
-    desired_bit_depth = 16  # 16-bit
+  # Set the desired sample rate and bit depth
+  desired_sample_rate = 16000  # 16 kHz
+  desired_bit_depth = 16  # 16-bit
 
-    # Resample and set bit depth
-    audio = audio.set_frame_rate(desired_sample_rate).set_sample_width(desired_bit_depth // 8)
+  # Resample and set bit depth
+  audio = audio.set_frame_rate(desired_sample_rate).set_sample_width(desired_bit_depth // 8)
 
-    # Export the converted audio
-    audio.export("user_downsampled.wav", format="wav")
-    # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
-    
-    speech_config.speech_recognition_language="fr-FR"
-    audio_config = speechsdk.AudioConfig(filename="user_downsampled.wav")
-    recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
+  # Export the converted audio
+  audio.export("user_downsampled.wav", format="wav")
+  # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+  
+  speech_config.speech_recognition_language="fr-FR"
+  audio_config = speechsdk.AudioConfig(filename="user_downsampled.wav")
+  recognizer = speechsdk.SpeechRecognizer(speech_config=speech_config, audio_config=audio_config)
 
-    # Start recognition
-    print("Speak into your microphone...")
-    speech_recognition_result = recognizer.recognize_once_async().get()
-    # Handle recognition result
-    if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
-        print("You: {}".format(speech_recognition_result.text))
-        print("Recognized: {}".format(speech_recognition_result.duration))
-        speaker_profile = {"Speaker": "This User", "Duration": speech_recognition_result.duration, "Speaking Rate": speech_recognition_result.duration/len(speech_recognition_result.text)}
-        agent_history["User Information"] += [{"Speaker Profile": speaker_profile, "Speaker Input": speech_recognition_result.text}]
-        user_input_text = speech_recognition_result.text
-    elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
-        print("No speech could be recognized: {}".format(speech_recognition_result.no_match_details))
-    elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
-        cancellation_details = speech_recognition_result.cancellation_details
-        print("Speech Recognition canceled: {}".format(cancellation_details.reason))
-        if cancellation_details.reason == speechsdk.CancellationReason.Error:
-            print("Error details: {}".format(cancellation_details.error_details))
-            print("Did you set the speech resource key and region values?")
-    
+  # Start recognition
+  print("Speak into your microphone...")
+  speech_recognition_result = recognizer.recognize_once_async().get()
+  # Handle recognition result
+  if speech_recognition_result.reason == speechsdk.ResultReason.RecognizedSpeech:
+    print("You: {}".format(speech_recognition_result.text))
+    print("Recognized: {}".format(speech_recognition_result.duration))
+    speaker_profile = {"Speaker": "This User", "Duration": speech_recognition_result.duration, "Speaking Rate": speech_recognition_result.duration/len(speech_recognition_result.text)}
+    agent_history["User Information"] += [{"Speaker Profile": speaker_profile, "Speaker Input": speech_recognition_result.text}]
+    user_input_text = speech_recognition_result.text
+  elif speech_recognition_result.reason == speechsdk.ResultReason.NoMatch:
+    print("No speech could be recognized: {}".format(speech_recognition_result.no_match_details))
+  elif speech_recognition_result.reason == speechsdk.ResultReason.Canceled:
+    cancellation_details = speech_recognition_result.cancellation_details
+    print("Speech Recognition canceled: {}".format(cancellation_details.reason))
+    if cancellation_details.reason == speechsdk.CancellationReason.Error:
+      print("Error details: {}".format(cancellation_details.error_details))
+      print("Did you set the speech resource key and region values?")
+  
 
-    user_sr = detect_sr()
+  user_sr = detect_sr()
 
-    print("User speech rate in syl/sec :: ", user_sr)
-    print("Finished.")
-    # TODO: We clean up the audio files, but should we consider archiving them, and make them part of the download the user
-    # give us?
-    delete_file("./user_downsampled.wav")
-    return (user_sr, user_input_text)
+  print("User speech rate in syl/sec :: ", user_sr)
+  print("Finished.")
+  # TODO: We clean up the audio files, but should we consider archiving them, and make them part of the download the user
+  # give us?
+  delete_file("./user_downsampled.wav")
+  return (user_sr, user_input_text)
 
 # %% [markdown]
 # ## Misc. functions
@@ -103,22 +103,22 @@ def recognize_speech(agent_history):
 import shutil
 import os
 def delete_file(file_path):
-    try:
-        os.remove(file_path)
-        print(f"File {file_path} deleted successfully.")
-    except FileNotFoundError:
-        print(f"File {file_path} not found.")
-    except PermissionError:
-        print(f"Permission denied: {file_path}.")
-    except Exception as e:
-        print(f"Error occurred while deleting file {file_path}: {e}")
+  try:
+    os.remove(file_path)
+    print(f"File {file_path} deleted successfully.")
+  except FileNotFoundError:
+    print(f"File {file_path} not found.")
+  except PermissionError:
+    print(f"Permission denied: {file_path}.")
+  except Exception as e:
+    print(f"Error occurred while deleting file {file_path}: {e}")
 
 def copy_file (dst: str, src: str) :
-    try:
-        shutil.copy(src, dst)  # Preserves metadata like timestamps
-        print(f"File copied successfully from {src} to {dst}")
-    except Exception as e:
-        print(f"An error occurred: {e}")
+  try:
+    shutil.copy(src, dst)  # Preserves metadata like timestamps
+    print(f"File copied successfully from {src} to {dst}")
+  except Exception as e:
+    print(f"An error occurred: {e}")
 
 
 # %% [markdown]
@@ -133,39 +133,39 @@ import io
 import sys
 
 def detect_sr() -> int:
-    # Create a StringIO object to capture the output
-    copy_file('./myprosody/myprosody/dataset/audioFiles/', 'user_input.wav')
+  # Create a StringIO object to capture the output
+  copy_file('./myprosody/myprosody/dataset/audioFiles/', 'user_input.wav')
 
 
-    p='user_input'
-    # c=r"INSERT A PATH" # YOU NEED TO INSERT YOUR LOCAL PATH. Most likely will have to move the string variable to the env?
-    # Here is what this line looks like for Abhi:
-    c=r"/home/jayabbhi/Documents/HCI_grad_project/sts/hci-rava-project/myprosody/myprosody"
-    # direct to the 2nd myprosody dir, but NO / at the end!
+  p='user_input'
+  # c=r"INSERT A PATH" # YOU NEED TO INSERT YOUR LOCAL PATH. Most likely will have to move the string variable to the env?
+  # Here is what this line looks like for Abhi:
+  c=r"/home/jayabbhi/Documents/HCI_grad_project/sts/hci-rava-project/myprosody/myprosody"
+  # direct to the 2nd myprosody dir, but NO / at the end!
 
-    captured_output = io.StringIO()
-    sys.stdout = captured_output  # Redirect sys.stdout to the StringIO object
-    try:
-        # Call the function whose output you want to capture
-        mysp.myspsr(p,c)
-    finally:
-        sys.stdout = sys.__stdout__  # Restore the original sys.stdout
-        
-    # Get the captured output as a string
-    output = captured_output.getvalue()
-    captured_output.close()  # Close the StringIO object
-    
-    final_syl_sec = 8
-    try:
-        final_syl_sec = int(output.split(" ")[1].strip())
-    except Exception as e:
-        print(output)
-    finally:
-        delete_file("./myprosody/myprosody/dataset/audioFiles/user_input.wav")
-        delete_file("./myprosody/myprosody/dataset/audioFiles/user_input.TextGrid")
-        delete_file("./user_input.wav")
-    
-    return final_syl_sec
+  captured_output = io.StringIO()
+  sys.stdout = captured_output  # Redirect sys.stdout to the StringIO object
+  try:
+    # Call the function whose output you want to capture
+    mysp.myspsr(p,c)
+  finally:
+    sys.stdout = sys.__stdout__  # Restore the original sys.stdout
+      
+  # Get the captured output as a string
+  output = captured_output.getvalue()
+  captured_output.close()  # Close the StringIO object
+  
+  final_syl_sec = 8
+  try:
+    final_syl_sec = int(output.split(" ")[1].strip())
+  except Exception as e:
+    print(output)
+  finally:
+    delete_file("./myprosody/myprosody/dataset/audioFiles/user_input.wav")
+    delete_file("./myprosody/myprosody/dataset/audioFiles/user_input.TextGrid")
+    delete_file("./user_input.wav")
+  
+  return final_syl_sec
 
 # %% [markdown]
 # ## Language Model
@@ -174,42 +174,38 @@ def detect_sr() -> int:
 # ### OpenAI GPT
 
 def generate_response(prompt, messages):
-    messages.append({"role": "user", "content": prompt})
+  messages.append({"role": "user", "content": prompt})
 
-    """Generate a response using Azure OpenAI Service."""
-    response = client.chat.completions.create(
-        messages=messages,
-        max_tokens=600,
-        temperature=1.0,
-        top_p=1.0,
-        model=deployment
-    )
+  """Generate a response using Azure OpenAI Service."""
+  response = client.chat.completions.create(
+      messages=messages,
+      max_tokens=600,
+      temperature=1.0,
+      top_p=1.0,
+      model=deployment
+  )
 
-    messages.append({"role": "system", "content": response.choices[0].message.content})
-     
-    return response.choices[0].message.content
+  messages.append({"role": "system", "content": response.choices[0].message.content})
+    
+  return response.choices[0].message.content
 
 # %%
+# TODO: implement the ollama version of the above code? It'd be faster AND use less credits when testing
+#       if we decide to do any of the study in-person
 
 
 '''We are going to work in percentages of the final SpeechSynthesizer'''
 def calc_new_sr_p(agent_sr_p, agent_sr, new_sr):
-    # TODO: need to setup a flag in the app, to determine the user's default speech rate.
-    user_sr_p = (new_sr / agent_sr) * agent_sr_p
-    new_agent_sr_p = int((agent_sr_p + user_sr_p) / 2.0)
+  user_sr_p = (new_sr / agent_sr) * agent_sr_p
+  new_agent_sr_p = int((agent_sr_p + user_sr_p) / 2.0)
 
-    new_agent_sr_p = max(1, new_agent_sr_p)
-    new_agent_sr_p = min(new_agent_sr_p, 15)
+  new_agent_sr_p = max(1, new_agent_sr_p)
+  new_agent_sr_p = min(new_agent_sr_p, 15)
 
-    # now that we have the new sr p for the agent, apply the same delta to the agent_sr
-    new_agent_sr = int((new_agent_sr_p / agent_sr_p) * agent_sr)
-    return new_agent_sr, new_agent_sr_p
+  # now that we have the new sr p for the agent, apply the same delta to the agent_sr
+  new_agent_sr = int((new_agent_sr_p / agent_sr_p) * agent_sr)
+  return new_agent_sr, new_agent_sr_p
 
-
-
-
-# %%
-# TODO: implement the ollama version of the above code, to make testing the program easier without using too many credits
 
 # %%
 def speak_response(response, sr_p):
@@ -219,7 +215,8 @@ def speak_response(response, sr_p):
     # Define SSML with Speaking Rate
     # TODO: Need to set a bounds on the speech rate. 5% matches pretty nicely to 8 syllables per second 
     # like Google's own TTS service. 20% is a little two fast, so let's cap it at 15%
-	rate = f'{sr_p}%' 
+	rate = f'{sr_p}%'
+	print(f'Synthesizer Rate: {rate}')
 	speech_config.speech_synthesis_voice_name = "fr-FR-VivienneMultilingualNeural"
 	ssml_string = f"""<speak version="1.0" xmlns="http://www.w3.org/2001/10/synthesis" 
     xmlns:mstts="http://www.w3.org/2001/mstts" 
